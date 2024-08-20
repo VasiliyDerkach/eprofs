1c1646233e978e0611637264b6a6fda4e2c9cc09
1 <v_derkach@inbox.ru>
2024-08-20 13:00:55 +0500

    20-08-24+пп1

--- LoadPlatCrm.py
+++ LoadPlatCrm.py
@@ -37,14 +37,14 @@ if __name__=='__main__':
         print('Не найден файл cnf')
     elif allfind:
         i,j,g,nid = 0,0,0,0
-        # try:
-        #     cnx = pymysql.connect(user=path_config['sql_login='], password=path_config['sql_password='],
-        #                               host=path_config['sql_server='],port=3306,  #path_config['sql_port='],
-        #                               database=path_config['sql_basename='],cursorclass=pymysql.cursors.DictCursor)
-        #
-        #     cur = cnx.cursor()
-        # except Exception as Exsql:
-        #     print(Exsql)
+        try:
+            cnx = pymysql.connect(user=path_config['sql_login='], password=path_config['sql_password='],
+                                      host=path_config['sql_server='],port=3306,  #path_config['sql_port='],
+                                      database=path_config['sql_basename='],cursorclass=pymysql.cursors.DictCursor)
+
+            cur = cnx.cursor()
+        except Exception as Exsql:
+            print(Exsql)
 
         #ftpfile = ftplib.FTP()
         #ftpfile = ftplib.FTP_TLS()
@@ -69,7 +69,8 @@ if __name__=='__main__':
         lst_scan = os.listdir(path_config['path_scan='])
         for gfile in lst_scan:
             exx = gfile[-4:]
-            if exx in ('.rtf','.pdf') and gfile[0:1]!='==':
+            if exx in ('.rtf'""",'.pdf'""") and gfile[0:1]!='==':
+                # для pdf учеть другие теги данных платежким и перенести теги для каждого типа файла в config
                 i += 1
                 txtfile_name = gfile
                 txtfile_name_path = path_config['path_scan='] + txtfile_name
@@ -91,34 +92,46 @@ if __name__=='__main__':
                         doc_id = doc_id.group(0)
                         if len(doc_id) == 36:
                             print('doc_id=', doc_id)
-                    txt = txt_id.split('\n')
-                    #print(txt[17])
-                    txt17 = txt[17].replace('\\','~')
-                    pdata = re.search(f'(?<=~pard ~fi0 ~li0 ~ri0 ~sb0 ~sa0 ~cb1 ~qc~sl240 ~slmult1 ~f0~fs20~cf2 ).*?(?={str_end})', txt17)
-                    if pdata:
-                        pdata = pdata.group(0)
-                    print(pdata)
-                    txt19 = txt[19].replace('\\','~')
-                    print(txt19)
-                    pnum = re.search(f'(?<=~pard ~fi0 ~li0 ~ri0 ~sb0 ~sa0 ~cb1 ~ql~sl240 ~slmult1 ~f0~fs20~cf2 ).*?(?={str_end})', txt17)
-                    if pnum:
-                        pnum = pnum.group(0)
-                    print(pnum)
-                    #print(txt[19])
-                    #print(txt[119])
+                            if LoadDocsCrm.is_valid_uuid(doc_id):
+                                print(f'Валидный {doc_id}')
+                                cur.execute(f"select id from documents where id='{doc_id}' limit 1")
+                                id_lst = cur.fetchall()
+                                id_rez=id_lst[0]['id']
+                                if len(id_rez)>0 and id_rez.upper()==doc_id.upper():
+                                    uu_id = uuid.uuid4().urn.replace('urn:uuid:','').upper()
+                                    os.rename(path_config['path_scan=']+gfile,path_config['path_scan=']+uu_id)
+                                    dnow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                                    sql_ins = f"insert into document_revisions(id, date_entered, change_log, document_id, filename, file_ext, file_mime_type, stage) values('{uu_id}','{dnow}','{pg1}','{doc_id1}','{old_fname}','{exx0}','{exx1}','scan in pyth')"
+                                    try:
+                                        cur.execute(sql_ins)
+                                        cnx.commit()
+                                    except:
+                                        cnx.rollback()
+
+                                    txt = txt_id.split('\n')
+                                    #print(txt[17])
+                                    txt17 = txt[17].replace('\\','~')
+                                    pdata = re.search(f'(?<=~pard ~fi0 ~li0 ~ri0 ~sb0 ~sa0 ~cb1 ~qc~sl240 ~slmult1 ~f0~fs20~cf2 ).*?(?={str_end})', txt17)
+                                    if pdata:
+                                        pdata = pdata.group(0)
+                                    print(pdata)
+                                    txt19 = txt[19].replace('\\','~')
+                                    #print(txt19)
+                                    pnum = re.search(f'(?<=~pard ~fi0 ~li0 ~ri0 ~sb0 ~sa0 ~cb1 ~ql~sl240 ~slmult1 ~f0~fs20~cf2 ).*?(?={str_end})', txt19)
+                                    if pnum:
+                                        pnum = pnum.group(0)
+                                    print(pnum)
+                                    txt119 = txt[119].replace('\\','~')
+                                    #print(txt19)
+                                    psum = re.search(f'(?<=~pard ~fi0 ~li0 ~ri0 ~sb0 ~sa0 ~cb1 ~ql~sl240 ~slmult1 ~f0~fs20~cf2 ).*?(?=={str_end})', txt119)
+                                    if psum:
+                                        psum = psum.group(0)
+                                    print(psum)
+                                #print(txt[19])
+                                #print(txt[119])
 
                     if True:
                         pass
-                        #cur.execute(f"select id from documents where id='{doc_id1}' limit 1")
-                        #id_lst = cur.fetchall()
-                        #os.rename(path_config['path_scan=']+gfile,path_config['path_scan=']+uu_id)
-                        #dnow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        #sql_ins = f"insert into document_revisions(id, date_entered, change_log, document_id, filename, file_ext, file_mime_type, stage) values('{uu_id}','{dnow}','{pg1}','{doc_id1}','{old_fname}','{exx0}','{exx1}','scan in pyth')"
-                        # try:
-                        #     cur.execute(sql_ins)
-                        #     cnx.commit()
-                        # except:
-                        #     cnx.rollback()
                         # if on_ftp:
                         #         with open(path_config['path_scan=']+uu_id,'rb') as gfile_n:
                         #             ftpfile.storbinary('STOR ' +uu_id,gfile_n)
@@ -149,8 +162,8 @@ if __name__=='__main__':
             print(f'Для {nid} файлов не найден в БД документ с нужным id или такой документ был удален из БД')
         # if on_ftp:
         #     ftpfile.quit()
-        # cur.close()
-        # cnx.close()
+        cur.close()
+        cnx.close()
     # h = 'tytyty.jpg'
     # print(h[0:-4])
     # uuidtest = 'c6f66f3c-eb06-ecda-8d22-66bb47328469--'
