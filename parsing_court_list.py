194af4ae9865e9c8c8d2163860316e0efbde6549
1 <v_derkach@inbox.ru>
2024-08-29 16:55:25 +0500

    29-08-2024 work+upd

--- parsing_court_list.py
+++ parsing_court_list.py
@@ -42,19 +42,20 @@ if __name__=='__main__':
     for crt in ctabl:
         wb = crt['website'].replace('http://', '')
         wb = wb.replace('https://', '')
-        sq = f"select id from accounts where deleted=0 and website='{wb}' and account_as_parent_child_id='{id_up}' order by date_modified desc"
-        print(sq)
+        #sq = f"select id from accounts where deleted=0 and website='{wb}' and account_as_parent_child_id='{id_up}' order by date_modified desc"
+        sq = f"select id from accounts where deleted=0 and website='{wb}' order by date_modified desc"
+        #print(sq)
         cur.execute(sq)
         lst = cur.fetchall()
+        id_reg = db_eprof.get_city_id_in_db(cur, 'regions', crt['legal_street'][1], '1')
+        id_reg = id_reg[0]
+        id_cit = db_eprof.get_city_id_in_db(cur, 'cities', crt['legal_street'][2], id_reg[:2])
+        id_munic = id_cit[1]
+        id_cit = id_cit[0]
         llst = len(lst)
         if not llst>0:
             uu_id = uuid.uuid4().urn.replace('urn:uuid:', '')
             dnow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            id_reg = db_eprof.get_city_id_in_db(cur,'regions',crt['legal_street'][1],'1')
-            id_reg = id_reg[0]
-            id_cit  = db_eprof.get_city_id_in_db(cur,'cities',crt['legal_street'][2],id_reg[:2])
-            id_munic = id_cit[1]
-            id_cit = id_cit[0]
 
             sqli = f'insert into accounts (id,date_entered,date_modified,name,legal_entity_name,account_as_parent_child_id,type,phone_office,'
             sqli = sqli + f"industry,assigned_team_id,legal_country_id,actual_country_id,legal_region_id,actual_region_id,legal_municipal_id,"
@@ -64,7 +65,16 @@ if __name__=='__main__':
             sqli = sqli + f"'{id_cit}',{id_cit}','{crt['legal_street'][3]}','{crt['legal_street'][3]}','{crt['legal_street'][4]}','{crt['legal_street'][4]}',"
             sqli = sqli + f"'{crt['legal_street'][0]}','{crt['legal_street'][0]}','{wb}'"
             s = f"call SetEmailContact('{uu_id}','{crt['email']}','1')"
-            print(sqli)
-            print(s)
+            # print(sqli)
+            # print(s)
         else:
             print(f'Найден {crt}')
+            s1 = f"select name, legal_entity_name,phone_officeassigned_team_id,legal_country_id,actual_country_id,legal_region_id,"
+            s1 = s1 + f"actual_region_id,legal_municipal_id,actual_municipal_id,legal_city_id,actual_city_id,legal_street,actual_street,"
+            s1 = s1 + f"legal_house,actual_house,legal_postalcode,actual_postalcode from accounts where id='{lst[0]['id']}'"
+            cur.execute(sq)
+            lst_f = cur.fetchall()
+            for k,v in lst_f[0]:
+                print(k,v)
+    cur.close()
+    cnx.close()
