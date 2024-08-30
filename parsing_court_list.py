0df03093f221097f92f1ae24c1dcc663a548a9e1
1 <v_derkach@inbox.ru>
2024-08-30 10:50:06 +0500

    30-08-2024

--- parsing_court_list.py
+++ parsing_court_list.py
@@ -44,8 +44,8 @@ if __name__=='__main__':
         wb = wb.replace('https://', '')
         #sq = f"select id from accounts where deleted=0 and website='{wb}' and account_as_parent_child_id='{id_up}' order by date_modified desc"
         sq = f"select id from accounts where deleted=0 and website like '%{wb}%' order by date_modified desc"
-        if crt['website']=='http://oktiabrsky.svd.sudrf.ru':
-            print(sq)
+        # if crt['website']=='http://oktiabrsky.svd.sudrf.ru':
+        #     print(sq)
         cur.execute(sq)
         lst = cur.fetchall()
         id_reg = db_eprof.get_city_id_in_db(cur, 'regions', crt['legal_street'][1], '1')
@@ -54,6 +54,7 @@ if __name__=='__main__':
         id_munic = id_cit[1]
         id_cit = id_cit[0]
         llst = len(lst)
+        s = None
         if not llst>0:
             uu_id = uuid.uuid4().urn.replace('urn:uuid:', '')
             dnow = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -63,8 +64,8 @@ if __name__=='__main__':
             sqli = sqli + f"actual_municipal,legal_city,actual_city,legal_street,actual_street,legal_house,actual_house,"
             sqli = sqli + f"legal_postalcode,actual_postalcode,website) values('{uu_id}','{dnow}','{dnow}','{crt['name']}','{crt['name']}',"
             sqli = sqli + f"'{id_up}','court','{crt['phone_office']}','court1','{id_tm}','1','1','{id_reg}','{id_reg}','{id_munic}','{id_munic}',"
-            sqli = sqli + f"'{id_cit}',{id_cit}','{crt['legal_street'][3]}','{crt['legal_street'][3]}','{crt['legal_street'][4]}','{crt['legal_street'][4]}',"
-            sqli = sqli + f"'{crt['legal_street'][0]}','{crt['legal_street'][0]}','{wb}'"
+            sqli = sqli + f"'{id_cit}','{id_cit}','{crt['legal_street'][3]}','{crt['legal_street'][3]}','{crt['legal_street'][4]}','{crt['legal_street'][4]}',"
+            sqli = sqli + f"'{crt['legal_street'][0]}','{crt['legal_street'][0]}','{wb}')"
             s = f"call SetEmailContact('{uu_id}','{crt['email']}','1')"
             # print(sqli)
             # print(s)
@@ -73,10 +74,37 @@ if __name__=='__main__':
             # s1 = f"select name, legal_entity_name,phone_office,assigned_team_id,legal_country_id,actual_country_id,legal_region_id,"
             # s1 = s1 + f"actual_region_id,legal_municipal_id,actual_municipal_id,legal_city_id,actual_city_id,legal_street,actual_street,"
             # s1 = s1 + f"legal_house,actual_house,legal_postalcode,actual_postalcode from accounts where id='{lst[0]['id']}'"
-            s1 = f"select * from accounts where id='{lst[0]['id']}'"
-            cur.execute(s1)
-            lst_f = cur.fetchall()
+            # s1 = f"select * from accounts where id='{lst[0]['id']}'"
+            # cur.execute(s1)
+            # lst_f = cur.fetchall()
+            sqli = f"update accounts set date_modified='{dnow}',name=if(name is null or name='','{crt['name']}',name),legal_entity_name='{crt['name']}'"
+            sqli = sqli + f",account_as_parent_child_id='{id_up}',type='court',phone_office='{crt['phone_office']}',industry='court1'"
+            sqli = sqli + f",assigned_team_id='{id_tm}',legal_country='1',actual_country='1',legal_region='{id_reg}',actual_region='{id_reg}'"
+            sqli= sqli + f",legal_municipal='{id_munic}',actual_municipal='{id_munic}',legal_city='{id_cit}',actual_city='{id_cit}'"
+            sqli = sqli + f",legal_street='{crt['legal_street'][3]}',actual_street='{crt['legal_street'][3]}'"
+            sqli = sqli + f",legal_house='{crt['legal_street'][4]}',actual_house='{crt['legal_street'][4]}',legal_postalcode='{crt['legal_street'][0]}',actual_postalcode='{crt['legal_street'][0]}',website='{wb}' "
+            sqli= sqli+ f"where id='{lst[0]['id']}'"
             # for k,v in lst_f[0].items():
             #     print(k,v)
+        try:
+            cur.execute(sqli)
+            cnx.commit()
+            if s:
+                try:
+                    cur.execute(s)
+                    cnx.commit()
+                    print(f'Выполнено  email для {crt['name']}')
+                except Exception as e:
+                    cnx.rollback()
+                    print(f'Ошибка email {e} для {crt['name']}')
+
+
+            print(f'Выполнено {'insert' if s else 'update'} для {crt['name']}')
+        except Exception as e:
+            cnx.rollback()
+            print(f'Ошибка  {e} в {'insert' if s else 'update'} для {crt['name']}')
+            print(sqli)
+
+
     cur.close()
     cnx.close()
