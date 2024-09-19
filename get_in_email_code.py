2d7c20b2f618718d4b42b4ac73cde023a3514c49
1 <v_derkach@inbox.ru>
2024-09-19 15:36:22 +0500

    1809-1

--- get_in_email_code.py
+++ get_in_email_code.py
@@ -2,6 +2,14 @@ import imaplib
 import email
 import re
 import deltatime_in_email
+import json
+
+with open("vkont_msg.json", "r") as rvkfile:
+    site_ifo = json.load(rvkfile)
+cod_email_str0 = site_ifo['cod_email_str0']
+cod_email_str1 = site_ifo['cod_email_str1']
+cod_email_subject = site_ifo['cod_email_subject']
+cod_email_str_after_username = site_ifo['cod_email_str_after_username']
 def get_in_email_code(imap_server,username,in_mail_name,password,priod_sec,now_timezone):
     # Создание подключения и авторизация
     mail = imaplib.IMAP4_SSL(imap_server)
@@ -20,58 +28,59 @@ def get_in_email_code(imap_server,username,in_mail_name,password,priod_sec,now_t
         if deltatm>priod_sec:
             continue
         subject = email.header.decode_header(msg["Subject"])[0][0].decode('utf-8')
-        fr = email.header.decode_header(msg["From"])
-        if fr:
-            print(fr)
-            try:
-                emailfrom = fr[1][0].decode('utf-8') #периодическая ошибка поиска
-            except:
-                print('В письме с кодом не найден From [1][0]')
-                print('fr[0]=',fr[0])
-                emailfrom = fr[0][0]#.decode('utf-8')
-                print('emailfrom=',emailfrom)
+        if subject==cod_email_subject:
+            fr = email.header.decode_header(msg["From"])
+            if fr:
+                print(fr)
+                try:
+                    emailfrom = fr[1][0].decode('utf-8') #периодическая ошибка поиска
+                except:
+                    print('В письме с кодом не найден From [1][0]')
+                    print('fr[0]=',fr[0])
+                    emailfrom = fr[0][0]#.decode('utf-8')
+                    print('emailfrom=',emailfrom)
 
-        else:
-            print(msg)
-        # print(f"Непрочитанное сообщение с темой: {subject}")
-        # print(f"от: {emailfrom} {emaildate}")
-        if msg.is_multipart():
-            #print("Multipart: Yes")
-            for part in msg.walk():
-                payload = part.get_payload(decode=True)#.decode()
-
-        else:
-            #print("Multipart: No")
-            payload = msg.get_payload(None)
-
-        txt = payload.split()
-        #print('txt=\n',txt.decode('utf-8'))
-        content_type = msg.get_content_type()
-        #print('content_type ',content_type)
-        po = None
-        pk1 = None
-        pk = None
-        for i,t in enumerate(txt):
-            tx = t.decode('utf-8')
-            pk = tx.find(in_mail_name + '&#33;<br><br>')
-
-            # pk = tx.find(in_mail_name )
-            # print(i,tx)
-            # if i==71:
-            #     print(pk)
-            if pk==0:
-                pk1 = pk
-                print(f'Найден {in_mail_name}')
             else:
-                print(f'Не найден {in_mail_name}')
-            po = re.search('(?<=' + 'авторизуетесь.<br><br><b><b>' + ').*?(?=</b></b><br><br><b>Если)', t.decode('utf-8'))
-            # брать строки из настройки
-            if po:
-                print(f'найден код {po.group(0)}')
-            if po and pk1==0:
-                return emailfrom,subject,emaildate,po.group(0)
-        if not po:
-            return None
+                print(msg)
+            # print(f"Непрочитанное сообщение с темой: {subject}")
+            # print(f"от: {emailfrom} {emaildate}")
+            if msg.is_multipart():
+                #print("Multipart: Yes")
+                for part in msg.walk():
+                    payload = part.get_payload(decode=True)#.decode()
+
+            else:
+                #print("Multipart: No")
+                payload = msg.get_payload(None)
+
+            txt = payload.split()
+            #print('txt=\n',txt.decode('utf-8'))
+            content_type = msg.get_content_type()
+            #print('content_type ',content_type)
+            po = None
+            pk1 = None
+            pk = None
+            for i,t in enumerate(txt):
+                tx = t.decode('utf-8')
+                pk = tx.find(in_mail_name + cod_email_str_after_username)
+
+                # pk = tx.find(in_mail_name )
+                # print(i,tx)
+                # if i==71:
+                #     print(pk)
+                if pk==0:
+                    pk1 = pk
+                    print(f'Найден {in_mail_name}')
+                else:
+                    print(f'Не найден {in_mail_name}')
+                po = re.search('(?<=' + cod_email_str0 + cod_email_str1, t.decode('utf-8'))
+                # брать строки из настройки
+                if po:
+                    print(f'найден код {po.group(0)}')
+                if po and pk1==0:
+                    return emailfrom,subject,emaildate,po.group(0)
+            if not po:
+                return None
 
     mail.logout()
 if __name__=='__main__':
